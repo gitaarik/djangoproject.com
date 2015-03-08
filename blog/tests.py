383522ad721e5e29d2e8fe9a6fd1e53b648084c8
Baptiste Mispelon <bmispelon@gmail.com>
2015-03-08 17:14:04 +0100

    Fixed #399 -- Hid past events from "upcoming events" list

--- blog/tests.py
+++ blog/tests.py
@@ -1,17 +1,20 @@
 from datetime import timedelta
 
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.utils import timezone
 
 from .models import Entry, Event
 
 
-class EntryTestCase(TestCase):
+class DateTimeMixin(object):
     def setUp(self):
         self.now = timezone.now()
         self.yesterday = self.now - timedelta(days=1)
         self.tomorrow = self.now + timedelta(days=1)
 
+
+class EntryTestCase(DateTimeMixin, TestCase):
     def test_manager_active(self):
         """
         Make sure that the Entry manager's `active` method works
@@ -33,12 +36,7 @@ class EntryTestCase(TestCase):
         self.assertEqual(Entry.objects.published().count(), 1)
 
 
-class EventTestCase(TestCase):
-    def setUp(self):
-        self.now = timezone.now()
-        self.yesterday = self.now - timedelta(days=1)
-        self.tomorrow = self.now + timedelta(days=1)
-
+class EventTestCase(DateTimeMixin, TestCase):
     def test_manager_past__future(self):
         """
         Make sure that the Entry manager's `active` method works
@@ -48,3 +46,15 @@ class EventTestCase(TestCase):
 
         self.assertEqual(Event.objects.future().count(), 1)
         self.assertEqual(Event.objects.past().count(), 1)
+
+
+class ViewsTestCase(DateTimeMixin, TestCase):
+    def test_no_past_upcoming_events(self):
+        """
+        Make sure there are no past event in the "upcoming events" sidebar (#399)
+        """
+        # We need a published entry on the index page so that it doesn't return a 404
+        Entry.objects.create(pub_date=self.yesterday, is_active=True)
+        Event.objects.create(date=self.yesterday, pub_date=self.now, is_active=True, headline='Jezdezcon')
+        response = self.client.get(reverse('weblog:index'))
+        self.assertNotContains(response, 'Jezdezcon', status_code=200)
