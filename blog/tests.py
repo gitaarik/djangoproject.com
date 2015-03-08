602c1e0032594080f0d1e36b67e94d8e90f05575
Baptiste Mispelon <bmispelon@gmail.com>
2015-03-08 17:05:05 +0100

    Added past/future methods on Event managers.

--- blog/tests.py
+++ blog/tests.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from django.test import TestCase
 from django.utils import timezone
 
-from .models import Entry
+from .models import Entry, Event
 
 
 class EntryTestCase(TestCase):
@@ -31,3 +31,20 @@ class EntryTestCase(TestCase):
         Entry.objects.create(pub_date=self.tomorrow, is_active=True)
 
         self.assertEqual(Entry.objects.published().count(), 1)
+
+
+class EventTestCase(TestCase):
+    def setUp(self):
+        self.now = timezone.now()
+        self.yesterday = self.now - timedelta(days=1)
+        self.tomorrow = self.now + timedelta(days=1)
+
+    def test_manager_past__future(self):
+        """
+        Make sure that the Entry manager's `active` method works
+        """
+        Event.objects.create(date=self.yesterday, pub_date=self.now)
+        Event.objects.create(date=self.tomorrow, pub_date=self.now)
+
+        self.assertEqual(Event.objects.future().count(), 1)
+        self.assertEqual(Event.objects.past().count(), 1)
